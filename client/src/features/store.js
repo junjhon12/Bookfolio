4ccd78c09ccfd2a2edf69fa0df8276e325072c2f
SophieNguyen113 <sophiengocnguyen113@gmail.com>
2025-03-27 20:35:40 -0400

    Built book store logic

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import booksReducer from "../features/booksSlice";
+import booksReducer from "./booksSlice";
 
 export const store = configureStore({
   reducer: {
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
-import { store } from "./app/store.js";
+import { store } from "./features/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
