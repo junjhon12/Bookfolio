8e2f9872df14a7f8b8f3041ba17116283d1ccaf7
SophieNguyen113 <sophiengocnguyen113@gmail.com>
2025-04-04 12:49:45 -0400

    Added structure, image, and text

--- client/src/sections/Header.jsx
+++ client/src/sections/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({user, api_url}) {
   return (
     <header className="header flex justify-between items-center p-2 px-12 fixed w-full top-0 left-0 z-10 bg-black text-white">
       <div className="flex items-center space-x-4">
-        <img src="/logo.svg" alt="Bookfolio Logo" className="w-12 h-12" />
+        <img src="./logo.svg" alt="Bookfolio Logo" className="w-12 h-12" />
         <h1 className="text-white text-xl">Bookfolio</h1>
       </div>
       <Nav user={user} api_url={api_url}/>
